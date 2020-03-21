379d03ab944de479e6a9889e7170cca0f6133d5f
Jan Krall <jan.krall@web.de>
2020-03-21 19:29:21 +0100

    added TestData

--- ClientApp/src/pages/Home/Home.tsx
+++ ClientApp/src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { CustomerMap, Button, ButtonTypes } from "../../components/index";
 import { addMarkets } from "../../store/marketActions";
-import { Market } from "../../types/Market";
+import { Market, MarketList } from "../../types/Market";
 import Api from "../../utils/FakeApi";
 import "./Home.styles.css";
 
@@ -20,9 +20,41 @@ export const Home = () => {
     });
   };
 
+  const MarketList: MarketList = {
+    Markets: [
+      {
+        Id: 12335,
+        Name: "Rewe um die Ecke",
+        Company: "Rewe",
+        Adresse: "Straße5",
+        GPSLocation: { lat: 52.520008, long: 13.404954 },
+        Status: 3, // 1 = leer; 2=mittel; 3=voll; null = undefiniert
+      },
+      {
+        Id: 2562,
+        Name: "Rewe um die Ecke",
+        Company: "Rewe",
+        Adresse: "Straße3",
+        GPSLocation: { lat: 52.600008, long: 13.404954 },
+        Status: 2, // 1 = leer; 2=mittel; 3=voll; null = undefiniert
+      },
+      {
+        Id: 2462,
+        Name: "Rewe um die Ecke",
+        Company: "Rewe",
+        Adresse: "Straße3",
+        GPSLocation: { lat: 52.500008, long: 13.404954 },
+        Status: 1, // 1 = leer; 2=mittel; 3=voll; null = undefiniert
+      }
+    ]
+  };
+
   return (
-    <div className='Home'>
-      <CustomerMap />
+    <div className="Home">
+      <CustomerMap
+        MarketList={MarketList}
+        MapCenter={{ lat: 52.520008, long: 13.404954 }}
+      />
     </div>
   );
 };
