4b9749d945609ae6217ed0531b657e4b72c23fc7
Jan Krall <jan.krall@web.de>
2020-03-21 14:43:24 +0100

    added TextField

---
+++ ClientApp/src/components/TextField/TextField.tsx
@@ -0,0 +1,43 @@
+import "./TextField.styles.css";
+import * as React from "react";
+import Form from "react-bootstrap/Form";
+
+export enum TextFieldTypes {
+  Password,
+  Normal
+}
+
+export interface TextFieldProps {
+  title?: string;
+  Type: TextFieldTypes;
+  onChange?: () => void;
+  className?: string;
+  children?: any;
+}
+
+export function TextField(props: TextFieldProps) {
+  if (props.Type === TextFieldTypes.Normal) {
+    return (
+      <Form.Group>
+        <Form.Label>{props.title}</Form.Label>
+        <Form.Control onChange={props.onChange} />
+      </Form.Group>
+    );
+  } else if (props.Type === TextFieldTypes.Password) {
+    return (
+      <Form.Group>
+        <Form.Label>{props.title}</Form.Label>
+        <Form.Control type="password" onChange={props.onChange} />
+      </Form.Group>
+    );
+  } else {
+      return (
+        <Form.Group>
+          <Form.Label>{props.title}</Form.Label>
+          <Form.Control onChange={props.onChange} />
+        </Form.Group>
+      );
+  }
+}
+
+export default TextField;
--- ClientApp/src/components/index.ts
+++ ClientApp/src/components/index.ts
@@ -1,4 +1,6 @@
 export { default as Button } from "./Button/Button";
 export { ButtonTypes } from "./Button/Button";
+export { default as TextField} from "./TextField/TextField"
+export { TextFieldTypes} from "./TextField/TextField"
 export { default as FetchData } from "./FetchData/FetchData";
 export { default as CustomerMap } from "./CustomerMap/CustomerMap";
